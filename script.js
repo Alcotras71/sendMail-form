dfab7eba9b3089125604e60a8687b8cdaecf98bb
Andrey Alekseev <60799652+Alcotras71@users.noreply.github.com>
2021-03-30 10:10:47 +0300

    init: init project

---
+++ script.js
@@ -0,0 +1,89 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+
+		if (error === 0) {
+
+		} else {
+			alert('Заполните обязательные поля');
+		}
+	}
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req');
+
+		for (let i = 0; i < formReq.length; i++) {
+			const input = formReq[i];
+			formRemoveError(input);
+
+			if (input.classList.contains('_email')) {
+				if (emailTest(input)) {
+					formAddError(input);
+					error++;
+				}
+			} else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+				formAddError(input);
+				error++;
+			} else {
+				if (input.value === '') {
+					formAddError(input);
+					error++;
+				}
+			}
+		}
+
+		return error;
+	}
+
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+
+	function emailTest(input) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+
+
+	const formImage = document.getElementById('formImage');
+	const formPreview = document.getElementById('formPreview');
+
+	formImage.addEventListener('change', () => {
+		uploadFile(formImage.files[0]);
+	});
+
+	function uploadFile(file) {
+		if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+			alert('Agreement only images');
+			formImage.value = '';
+			return;
+		}
+
+		// FileSize must be < 2Mb
+		if (file.size > 2 * 1024 * 1024) {
+			alert('File must be less than 2 mb');
+			return;
+		}
+
+		const reader = new FileReader();
+		reader.onload = function(e) {
+			formPreview.innerHTML = `<img src="${e.target.result}" alt='Photo'>`;
+		}
+		reader.onerror = function(e) {
+			alert('error');
+		}
+		reader.readAsDataURL(file);
+	}
+});
